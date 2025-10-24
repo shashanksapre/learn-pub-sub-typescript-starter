bb19bca9754504332c3a405754dd981f476c00da
Shashank Sapre <shashank.sapre@outlook.com>
2025-10-24 18:18:04 +0530

    message brokers

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,18 @@
+import amqp from "amqplib";
+
+const CONN_STRING = "amqp://guest:guest@localhost:5672/";
+
 async function main() {
   console.log("Starting Peril server...");
+
+  const conn = await amqp.connect(CONN_STRING);
+  console.log("connected to RabbitMQ");
+  process.on("SIGINT", async () => {
+    await conn.close();
+    console.log("RabbitMQ connection closed.");
+    console.log("shutting down...");
+    process.exit(0);
+  });
 }
 
 main().catch((err) => {
