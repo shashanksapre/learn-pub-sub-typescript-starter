391891dc2fe0c615b7a8cacd5f9a4f49911160a5
Shashank Sapre <shashank.sapre@outlook.com>
2025-10-28 22:09:49 +0530

    Decoupling

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,7 @@
 import amqp from "amqplib";
 import { publishJSON } from "../internal/pubsub/publish.js";
 import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+import { getInput, printServerHelp } from "../internal/gamelogic/gamelogic.js";
 
 const CONN_STRING = "amqp://guest:guest@localhost:5672/";
 
@@ -18,12 +19,35 @@ async function main() {
     {}
   );
 
-  try {
-    await publishJSON(publishCh, exchange.exchange, PauseKey, {
-      isPaused: true,
-    });
-  } catch (err) {
-    console.error("Error publishing message:", err);
+  printServerHelp();
+
+  while (1) {
+    try {
+      const inputs = await getInput("$$$@PR3 > ");
+      if (inputs.length == 0) {
+        continue;
+      }
+      if (inputs[0] === "pause") {
+        console.log("Sending a pause...");
+        await publishJSON(publishCh, exchange.exchange, PauseKey, {
+          isPaused: true,
+        });
+      } else if (inputs[0] === "resume") {
+        console.log("Sending a resume...");
+        await publishJSON(publishCh, exchange.exchange, PauseKey, {
+          isPaused: false,
+        });
+      } else if (inputs[0] === "quit") {
+        await conn.close();
+        console.log("RabbitMQ connection closed.");
+        console.log("shutting down...");
+        process.exit(0);
+      } else {
+        console.log("What?");
+      }
+    } catch (err) {
+      console.error("Error publishing message:", err);
+    }
   }
 
   process.on("SIGINT", async () => {
